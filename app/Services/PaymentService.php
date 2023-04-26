d788c0b361fa9d664cef360b3b364e8cf70a73e6
jccun23 <131837839+jccun23@users.noreply.github.com>
2023-04-26 12:42:58 +0800

    Added changes for the checkout logic and read me

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -2,10 +2,57 @@
 
 namespace App\Services;
 
+use App\Constants\StripeConstants;
+use App\Utilities\PGCompanyUtility;
+
 class PaymentService
 {
+    private $oPGCompanyUtility;
+
+    public function __construct(PGCompanyUtility $oPGCompanyUtility)
+    {
+        $this->oPGCompanyUtility = $oPGCompanyUtility;
+    }
+
     public function createCheckout(array $aCheckoutRequest)
     {
-        
+        $this->setupSettings();
+        $aCheckoutParams = $this->formatCheckoutRequest($aCheckoutRequest);
+        $aCheckoutResponse = $this->oPGCompanyUtility->createCheckout($aCheckoutParams);
+        if($aCheckoutResponse['result'] === 'failed') {
+            return $aCheckoutResponse;
+        }
+    }
+
+    private function setupSettings()
+    {
+        $this->oPGCompanyUtility->setupClient(env('API_KEY'));
+    }
+
+    private function formatCheckoutRequest(array $aParams)
+    {
+        return [
+            StripeConstants::SUCCESS_URL => '',
+            StripeConstants::CANCEL_URL  => '',
+            StripeConstants::LINE_ITEMS  => [
+                [
+                    StripeConstants::PRICE_DATA => [
+                        StripeConstants::PRODUCT_DATA => [
+                            StripeConstants::NAME => '',
+                            StripeConstants::UNIT_AMOUNT => '',
+                            StripeConstants::QUANTITY => 2,
+                        ]
+                    ] 
+                ]
+            ],
+            StripeConstants::MODE                => 'payment',
+            StripeConstants::PAYMENT_INTENT_DATA => [
+                StripeConstants::ON_BEHALF_OF  => '',
+                StripeConstants::TRANSFER_DATA => [
+                    StripeConstants::DESTINATION => ''
+                ]
+            ],
+            StripeConstants::PAYMENT_METHOD_TYPES => ['card']
+        ];
     }
 }
